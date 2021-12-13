7799ea6c20a26e12135b6b3e3d38006d7023aa90
Fredrik Hammar <cflhammar@gmail.com>
2021-12-13 07:50:32 +0100

    Dec 12 and Dec 13

--- Days/Dec13/index.ts
+++ Days/Dec13/index.ts
@@ -1,6 +1,5 @@
 import { readFileWithDirections } from "./Input/ReadFileWithDirections";
 import { paperFold } from "./src/paperFold";
-import { CaveSystem } from "./src/CaveSystem";
 
 const directions = readFileWithDirections("input.txt");
 console.log(paperFold(directions));
